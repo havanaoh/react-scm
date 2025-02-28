688b8e08a3d8a840958ab4835b6f3b96d6d8aeb2
havanaOh <top2480@naver.com>
2025-02-28 16:50:08 +0900

    공통코드 검색, 리스트 출력 구현

---
+++ src/api/Provider/CommonCodeProvider.tsx
@@ -0,0 +1,26 @@
+import React, { FC, createContext, useState } from "react";
+
+// 초기값의 타입.
+interface ISearchKeyword {
+    searchKeyword?: object;
+    setSearchKeyword?: React.Dispatch<React.SetStateAction<object>>;
+}
+
+// const defaultValue: ISearchKeyword = {
+//     searchKeyword: {},
+//     setSearchKeyword: () => {},
+// };
+
+// 다른 컴포넌트에서 사용이 가능한 값을 만든다.
+// export const CommonCodeContext = createContext(defaultValue);
+export const CommonCodeContext = createContext<ISearchKeyword>({});
+
+// 만들어진 값에 searchKeyword, setSearchKeyword을 넣어서 자식 노드에서 searchKeyword와, setSearchKeyword를 자유롭게 호출하게 한다.
+export const CommonCodeProvider: FC<{
+    children: React.ReactNode | React.ReactNode[];
+}> = ({ children }) => {
+    const [searchKeyword, setSearchKeyword] = useState({});
+    return (
+        <CommonCodeContext.Provider value={{ searchKeyword, setSearchKeyword }}>{children}</CommonCodeContext.Provider>
+    );
+};
--- src/components/page/Management/CommonCode/CommonCodeSearch/CommonCodeSearch.tsx
+++ src/components/page/Management/CommonCode/CommonCodeSearch/CommonCodeSearch.tsx
@@ -1,21 +1,42 @@
+import { useContext, useRef, useState } from "react";
 import { StyledButton } from "../../../../common/StyledButton/StyledButton";
+import { StyledSelectBox } from "../../../../common/StyledSelectBox/StyledSelectBox";
+import { StyledInput } from "../../../../common/StyledInput/StyledInput";
+import { CommonCodeSearchStyled } from "./styled";
+import { CommonCodeContext, CommonCodeProvider } from "../../../../../api/Provider/CommonCodeProvider";
 
 export const CommonCodeSearch = () => {
+    const [selectValue, setSelectValue] = useState<string>("groupName");
+    const inputValue = useRef<HTMLInputElement>();
+    // const test = useContext(CommonCodeContext);
+    // const test2 = useState({});
+    const { setSearchKeyword } = useContext(CommonCodeContext);
+    const options = [
+        { label: "그룹코드명", value: "groupName" },
+        { label: "그룹코드", value: "groupCode" },
+    ];
+
+    // console.log(test);
+    // console.log(test2);
+    const handlerSearch = () => {
+        setSearchKeyword({
+            groupCodeSelect: selectValue,
+            searchTitle: inputValue.current.value,
+        });
+    };
+
     return (
-        <div>
-            <div>
-                <div className='flex space-x-4'>
-                    <div>
-                        <select>
-                            <option value={"groupName"}>그룹코드명</option>
-                            <option value={"groupCode"}>그룹코드</option>
-                        </select>
-                        <input />
-                        <StyledButton variant='secondary'>검색</StyledButton>
-                        <StyledButton>등록</StyledButton>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <CommonCodeSearchStyled>
+            {/* <select>
+                <option value={"groupName"}>그룹코드명</option>
+                <option value={"groupCode"}>그룹코드</option>
+            </select> */}
+            <StyledSelectBox options={options} value={selectValue} onChange={setSelectValue} />
+            <StyledInput ref={inputValue} />
+            <StyledButton variant='secondary' onClick={handlerSearch}>
+                검색
+            </StyledButton>
+            <StyledButton>등록</StyledButton>
+        </CommonCodeSearchStyled>
     );
 };
